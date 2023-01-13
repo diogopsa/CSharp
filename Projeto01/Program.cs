b3fcbbe61befef8db0e7a1c35cd201ae71e9d07e
Diogo <65966727+diogopsa@users.noreply.github.com>
2023-01-12 23:47:58 -0300

    Add files via upload

---
+++ Projeto01/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ProgramaPrincipal
+{
+    public class ClasseUm
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+    public class ClasseUm
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+
+
+
+}
